ea41b624d21f41e39138ea9dfd6a44d502f2d38c
kennedy <lual.salma@gmail.com>
2023-03-29 21:02:54 -0600

    Refactors

--- core/testing/src/main/java/com/example/foods/core/testing/Rx.kt
+++ core/testing/src/main/java/com/example/foods/core/testing/Rx.kt
@@ -3,7 +3,7 @@ package com.example.foods.core.testing
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.observers.TestObserver
 
-fun <T : Any> Single<T>.testAndGetData() =
+fun <T : Any> Single<T>.testAndGetData(): T =
     this.test().await().assertNoErrors().assertComplete().values().first()
 
 fun <T : Any> Single<T>.completeAssertedTest(): TestObserver<T> =
